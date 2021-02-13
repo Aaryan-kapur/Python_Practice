d01a7fae5c78934c07dc84be3e71ac35f9eea454
Aaryan-Kapur <ak6392@bennett.edu.in>
2021-02-13 14:32:32 +0530

    Create basic_loops.py

---
+++ basic_loops.py
@@ -0,0 +1,5 @@
+# The provided code stub reads and integer, a  , from STDIN. For all non-negative integers from 0 to n , print .
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(0,n):
+        print(i*i)
